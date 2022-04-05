28a57681fe0517b320a052df543bded1e003fd61
DongHun, Kim <donghun.kdh@gmail.com>
2022-04-06 01:27:41 +0900

    초기 커밋.

---
+++ XDrawer/XDrawer.cs
@@ -0,0 +1,15 @@
+namespace XDrawer
+{
+    public partial class XDrawer : Form
+    {
+        public XDrawer()
+        {
+            InitializeComponent();
+        }
+
+        private void XDrawer_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
