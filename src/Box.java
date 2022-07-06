34481325f6885fc415fc1d6fceca8c6ee1fd2464
DongHun, Kim <donghun.kdh@gmail.com>
2022-07-06 20:30:57 +0900

    ADD: add Box class

---
+++ src/Box.java
@@ -0,0 +1,16 @@
+public class Box {
+
+  int x1;
+  int y1;
+  int x2;
+  int y2;
+
+  Box() {
+
+  }
+
+  Box(int x, int y) {
+    x1 = x2 = x;
+    y1 = y2 = y;
+  }
+}
--- src/DrawerView.java
+++ src/DrawerView.java
@@ -4,20 +4,32 @@ import java.awt.event.*;
 
 public class DrawerView extends JPanel implements MouseListener, MouseMotionListener {
 
-  int startX;
-  int startY;
+  static int MAX = 100;
+  Box pBox;
+  Box boxes[];
+  int nBox;
 
   DrawerView() {
-    startX = 0;
-    startY = 0;
-
+    pBox = null;
+    boxes = new Box[MAX];
+    nBox = 0;
     addMouseListener(this);
     addMouseMotionListener(this);
   }
 
   //  paint event 시 자동 호출
   public void paintComponent(Graphics g) {
+
     super.paintComponent(g);
+
+    for (int i = 0; i < nBox; i++) {
+      int minX = Math.min(boxes[i].x1, boxes[i].x2);
+      int minY = Math.min(boxes[i].y1, boxes[i].y2);
+      int width = Math.abs(boxes[i].x2 - boxes[i].x1);
+      int height = Math.abs(boxes[i].y2 - boxes[i].y1);
+
+      g.drawRect(minX, minY, width, height);
+    }
   }
 
   //  마우스 이벤트 리스너
@@ -26,18 +38,18 @@ public class DrawerView extends JPanel implements MouseListener, MouseMotionList
   }
 
   public void mousePressed(MouseEvent e) {
-    startX = e.getX();
-    startY = e.getY();
+    pBox = new Box(e.getX(), e.getY());
+    boxes[nBox++] = pBox;
   }
 
   public void mouseReleased(MouseEvent e) {
-    int endX = e.getX();
-    int endY = e.getY();
+    pBox.x2 = e.getX();
+    pBox.y2 = e.getY();
 
-    int minX = Math.min(startX, endX);
-    int minY = Math.min(startY, endY);
-    int width = Math.abs(endX - startX);
-    int height = Math.abs(endY - startY);
+    int minX = Math.min(pBox.x1, pBox.x2);
+    int minY = Math.min(pBox.y1, pBox.y2);
+    int width = Math.abs(pBox.x2 - pBox.x1);
+    int height = Math.abs(pBox.y2 - pBox.y1);
 
     Graphics g = getGraphics();
     g.drawRect(minX, minY, width, height);
@@ -56,15 +68,27 @@ public class DrawerView extends JPanel implements MouseListener, MouseMotionList
   }
 
   public void mouseDragged(MouseEvent e) {
-    int endX = e.getX();
-    int endY = e.getY();
-
-    int minX = Math.min(startX, endX);
-    int minY = Math.min(startY, endY);
-    int width = Math.abs(endX - startX);
-    int height = Math.abs(endY - startY);
+//    이전 그림 지우기
+    int minX = Math.min(pBox.x1, pBox.x2);
+    int minY = Math.min(pBox.y1, pBox.y2);
+    int width = Math.abs(pBox.x2 - pBox.x1);
+    int height = Math.abs(pBox.y2 - pBox.y1);
 
     Graphics g = getGraphics();
+    g.setXORMode(getBackground());
+    g.drawRect(minX, minY, width, height);
+    g.setPaintMode();
+
+//    새 그림 그리기
+    pBox.x2 = e.getX();
+    pBox.y2 = e.getY();
+
+    minX = Math.min(pBox.x1, pBox.x2);
+    minY = Math.min(pBox.y1, pBox.y2);
+    width = Math.abs(pBox.x2 - pBox.x1);
+    height = Math.abs(pBox.y2 - pBox.y1);
+
+    g.setColor(Color.black);
     g.drawRect(minX, minY, width, height);
   }
 }
